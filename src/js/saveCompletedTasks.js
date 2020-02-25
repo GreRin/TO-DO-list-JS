17396645e0a432d52d7c6420e64538be11f0a486
Ryhor Sidoryn <rigor1988@gmail.com>
2020-02-25 22:10:40 +0300

    feat: load copletedData to screen

--- src/js/saveCompletedTasks.js
+++ src/js/saveCompletedTasks.js
@@ -1,11 +1,20 @@
-export function saveCompletedTasks(completedItem) {
+export function saveCompletedTasks(completedTasks) {
+    const completedTasksTitleArr = [];
     const completedTasksArr = [];
+    const completedPriority = [];
+    const completedData = [];
 
-    for (let i = 0; i < completedItem.children.length; i++) {
-        completedTasksArr.push(completedItem.children[i].getElementsByTagName('p')[0].innerText);
+    for (let i = 0; i < completedTasks.children.length; i++) {
+        completedTasksArr.push(completedTasks.children[i].getElementsByTagName('p')[0].innerText);
+        completedTasksTitleArr.push(completedTasks.children[i].getElementsByTagName('h5')[0].innerText);
+        completedPriority.push(completedTasks.children[i].children[0].children[0].children[1].children[0].innerText);
+        completedData.push(completedTasks.children[i].children[0].children[0].children[1].children[1].innerText);
     }
 
     localStorage.setItem('completed', JSON.stringify({
+        completedTasksTitleArr,
         completedTasksArr,
+        completedPriority,
+        completedData,
     }));
 }
--- src/js/tasksLoader.js
+++ src/js/tasksLoader.js
@@ -4,7 +4,6 @@ import { bindTaskEvents } from './bindTaskEvents';
 export function tasksLoader() {
     const data = JSON.parse(localStorage.getItem('current'));
     const currentTasks = document.getElementById('currentTasks');
-    console.log(data);
     for (let i = 0; i < data.currentTasksTitleArr.length; i++) {
         const title = data.currentTasksTitleArr[i];
         const task = data.currentTasksArr[i];
@@ -13,5 +12,17 @@ export function tasksLoader() {
         const listItem = createNewTask(title, priority, time, task);
         currentTasks.appendChild(listItem);
         bindTaskEvents(listItem);
+    }
+
+    const completedData = JSON.parse(localStorage.getItem('completed'));
+    const completedTasks = document.getElementById('completedTasks');
+    for (let i = 0; i < completedData.completedTasksTitleArr.length; i++) {
+        const title = completedData.completedTasksTitleArr[i];
+        const task = completedData.completedTasksArr[i];
+        const priority = completedData.completedPriority[i];
+        const time = completedData.completedData[i];
+        const completedItem = createNewTask(title, priority, time, task);
+        completedTasks.appendChild(completedItem);
+        bindTaskEvents(completedItem);
     }
 }
